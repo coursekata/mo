a70454fc51b2c4580d0b43157fe0258fb21470ee
Adam Blake <adam@ablake.dev>
2023-09-26 16:20:57 -0700

    feat: add checker for determining if all data has been downloaded

--- mo/cli/main.py
+++ mo/cli/main.py
@@ -1,13 +1,14 @@
 import logging
+from datetime import datetime
 from pathlib import Path
 from typing import Annotated, Optional
 
 import typer
+from rich import print
 
 from mo.cli.logging import configure_logging
+from mo.core.check.manifest import ManifestChecker
 from mo.core.organize.data import DataOrganizer
-
-# from mo.core.check.manifest import ManifestChecker
 
 app = typer.Typer()
 
@@ -50,26 +51,31 @@ def organize(
     organize(input, output, keep_source)
 
 
-# @app.command()
-# def check(
-#     data: Annotated[Path, typer.Argument()],
-#     manifest: Annotated[Optional[Path], typer.Option("--manifest", "-m")] = None,
-# ) -> None:
-#     """Check that data has been downloaded for all classes in the manifest."""
-#     manifest = Path(data) / "manifest.csv" if not manifest else Path(manifest)
-#     if not manifest.exists():
-#         raise typer.BadParameter(f"Manifest not found at {manifest}")
+@app.command()
+def check(
+    data: Annotated[Path, typer.Argument()],
+    manifest: Annotated[Optional[Path], typer.Option("--manifest", "-m")] = None,
+) -> None:
+    """Check that data has been downloaded for all classes in the manifest."""
+    manifest = Path(data, "manifest.csv") if not manifest else Path(manifest)
+    if not manifest.exists():
+        raise typer.BadParameter(f"Manifest not found at {manifest}")
 
-#     print(
-#         "Checking against manifest dated ",
-#         datetime.fromtimestamp(manifest.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S"),
-#     )
+    organized = Path(data, "classes").with_suffix(".parquet")
+    if not organized.exists():
+        raise typer.BadParameter(f"Organized classes data not found at {organized}")
 
-#     missing = list(services.check(data, manifest))
-#     if not missing:
-#         print("[green]All classes have been downloaded![/green]")
-#         return
+    print(
+        "Checking data against manifest dated ",
+        datetime.fromtimestamp(manifest.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S"),
+    )
 
-#     print(f"[bold red]Missing data for {len(missing)} classes:[/bold red]")
-#     for id in services.check(data, manifest):
-#         print(f"[link=https://coursekata.org/research/classes/view/{id}]{id}[/link]")
+    checker = ManifestChecker()
+    missing = checker(manifest, organized)
+    if not missing:
+        print("[green]All classes have been downloaded![/green]")
+        return
+
+    print(f"[bold red]Missing data for {len(missing)} classes:[/bold red]")
+    for id in missing:
+        print(f"[link=https://coursekata.org/research/classes/view/{id}]{id}[/link]")
--- mo/core/check/manifest.py
+++ mo/core/check/manifest.py
@@ -1,13 +1,31 @@
 """Check that data has been downloaded for all completed classes in the manifest."""
 
-from typing import Any
+
+import polars as pl
 
 from mo.core.interfaces import IReader
 from mo.core.read.classes import ManifestReader
+from mo.core.typing import PathStr
 
 
 class ManifestChecker:
     manifest_reader: IReader = ManifestReader()
 
-    def __call__(self, manifest_path: Path, data_dir: Path) -> Any:
+    def __call__(self, manifest_path: PathStr, organized_path: PathStr) -> list[str]:
         """Check download folder for all completed classes in the manifest."""
+        organized_ids = pl.scan_parquet(organized_path).select("class_id").unique()
+
+        manifest_ids = (
+            self.manifest_reader(manifest_path)
+            .filter(pl.col("class_type") == "real")
+            .filter(pl.col("status") == "complete")
+            .select("class_id")
+            .unique()
+        )
+
+        return (
+            manifest_ids.join(organized_ids, on="class_id", how="anti")
+            .collect()
+            .to_series()
+            .to_list()
+        )
